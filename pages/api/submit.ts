d9c872ae2f505179a4d27dd2f74d7d511888acf7
ritvik <ritviksharma3@gmail.com>
2023-05-22 00:51:56 +0530

    prisma fix

--- pages/api/submit.ts
+++ pages/api/submit.ts
@@ -39,7 +39,7 @@ export default async function handler(
     // 	createdAt: new Date().toISOString(),
     // });
 
-    await db.tRANSPORTSONEPAT.create({
+    await db.transportSonepat.create({
       data: {
         name,
         email,
