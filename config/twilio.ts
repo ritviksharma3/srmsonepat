2970b4e1da43470ef8432d372139f3aaf1bffd9b
ritvik <ritviksharma3@gmail.com>
2023-05-26 15:01:53 +0530

    Next env var

--- config/twilio.ts
+++ config/twilio.ts
@@ -1,8 +1,8 @@
 import twilio from "twilio";
 
-const accountSid = "ACd3f6351338504eb13f8fade63f9064e4";
-const authToken = "d74a4a7b74ad00e60ee55783f2dbf3a1";
-const verifySid = "VA6577a1e681adf7c5a254d92235eb19d0";
+const accountSid = process.env.ACCOUNT_SID as string;
+const authToken = process.env.AUTH_TOKEN as string;
+const verifySid = process.env.VERIFY_SID as string;
 const client = twilio(accountSid, authToken);
 
 export { client, verifySid };
