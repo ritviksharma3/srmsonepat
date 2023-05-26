c7cd563f8cec57e37906b946121b2e7cd1c9b2b9
ritvik <ritviksharma3@gmail.com>
2023-05-26 10:26:47 +0530

    AuthToken Changed

--- config/twilio.ts
+++ config/twilio.ts
@@ -1,8 +1,8 @@
 import twilio from "twilio";
 
 const accountSid = "ACd3f6351338504eb13f8fade63f9064e4";
-const authToken = "5571260d96a1fd90ca571db5be86ea75";
-const verifySid = "VA0e1d22e3066b268e3dea49306a6f4ef9";
+const authToken = "d74a4a7b74ad00e60ee55783f2dbf3a1";
+const verifySid = "VA6577a1e681adf7c5a254d92235eb19d0";
 const client = twilio(accountSid, authToken);
 
 export { client, verifySid };
